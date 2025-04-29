7420d779f09c62c69e67bc72b395808529ed204d
computershawn <shontek@gmail.com>
2025-04-29 01:42:45 -0700

    Create canvas layers

--- src/components/Artboard.tsx
+++ src/components/Artboard.tsx
@@ -1,15 +1,76 @@
 import { useEffect, useRef } from 'react';
 
 const Artboard = () => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const compRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
-    if (canvasRef?.current !== null) {
-      const ctx = canvasRef.current.getContext('2d');
-      const wd = canvasRef.current.width;
-      const ht = canvasRef.current.height;
+    if (compRef?.current !== null) {
+      const ctx = compRef.current.getContext('2d');
+      const wd = compRef.current.width;
+      const ht = compRef.current.height;
       const num = 40;
       if (ctx !== null) {
+        ctx.strokeStyle = 'red';
+        for (let i = 0; i < num; i++) {
+          const x = Math.round(Math.random() * wd);
+          const y = Math.round(Math.random() * ht);
+          const r = 8 + Math.round(Math.random() * 32);
+          ctx.stroke();
+          ctx.beginPath();
+          ctx.arc(x, y, r, 0, 2 * Math.PI);
+          ctx.stroke();
+        }
+      }
+    }
+  }, []);
+
+  return (
+    <>
+      {/* Composition */}
+      <canvas
+        width='1280'
+        height='720'
+        ref={compRef}
+        style={{ position: 'absolute', top: 0, left: 300 }}
+      >
+        Your browser does not support the HTML canvas tag.
+      </canvas>
+
+      <Bezier />
+    </>
+  );
+};
+
+const Bezier = () => {
+  const bezierGeomRef = useRef<HTMLCanvasElement | null>(null);
+  const bezierCtrlRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    if (bezierGeomRef?.current !== null) {
+      const ctx = bezierGeomRef.current.getContext('2d');
+      const wd = bezierGeomRef.current.width;
+      const ht = bezierGeomRef.current.height;
+      const num = 40;
+      if (ctx !== null) {
+        ctx.strokeStyle = 'darkblue';
+        for (let i = 0; i < num; i++) {
+          const x = Math.round(Math.random() * wd);
+          const y = Math.round(Math.random() * ht);
+          const r = 8 + Math.round(Math.random() * 32);
+          ctx.beginPath();
+          ctx.arc(x, y, r, 0, 2 * Math.PI);
+          ctx.stroke();
+        }
+      }
+    }
+
+    if (bezierCtrlRef?.current !== null) {
+      const ctx = bezierCtrlRef.current.getContext('2d');
+      const wd = bezierCtrlRef.current.width;
+      const ht = bezierCtrlRef.current.height;
+      const num = 40;
+      if (ctx !== null) {
+        ctx.strokeStyle = 'blue';
         for (let i = 0; i < num; i++) {
           const x = Math.round(Math.random() * wd);
           const y = Math.round(Math.random() * ht);
@@ -22,21 +83,28 @@ const Artboard = () => {
     }
   }, []);
 
-  // return (
-  //     <>
-  //         <Box w={1280} h={720}>hello there</Box>
-  //     </>
-  // )
   return (
-    <canvas
-      id='myCanvas'
-      width='1280'
-      height='720'
-      ref={canvasRef}
-      //   style={{'border:1px solid #d3d3d3'}}
-    >
-      Your browser does not support the HTML canvas tag.
-    </canvas>
+    <>
+      {/* Bezier geometry */}
+      <canvas
+        width='1280'
+        height='720'
+        ref={bezierGeomRef}
+        style={{ position: 'absolute', top: 0, left: 300 }}
+      >
+        Your browser does not support the HTML canvas tag.
+      </canvas>
+
+      {/* Bezier controls */}
+      <canvas
+        width='1280'
+        height='720'
+        ref={bezierCtrlRef}
+        style={{ position: 'absolute', top: 0, left: 300 }}
+      >
+        Your browser does not support the HTML canvas tag.
+      </canvas>
+    </>
   );
 };
 
