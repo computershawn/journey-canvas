8d51409f624b9771aaf476e1a9d8d4e6ab686864
computershawn <shontek@gmail.com>
2025-05-04 18:07:11 -0700

    Drag and drop of control points

--- src/components/BeziControlsAlt.tsx
+++ src/components/BeziControlsAlt.tsx
@@ -1,68 +1,54 @@
 import React, { useState, useRef, useEffect } from 'react';
 
+const rad = 16;
+const pointRad = 3;
+
 const BeziControlsAlt = () => {
   const canvasRef = useRef(null);
-  const [isDragging, setIsDragging] = useState(false);
+  // const [isDragging, setIsDragging] = useState(false);
   const [offsetX, setOffsetX] = useState(0);
   const [offsetY, setOffsetY] = useState(0);
-  const [shape, setShape] = useState({
-    x: 50,
-    y: 50,
-    width: 50,
-    height: 50,
-    isDragging: false,
-  });
+  // const [shape, setShape] = useState({
+  //   x: 50,
+  //   y: 50,
+  //   width: 50,
+  //   height: 50,
+  // });
   const [dragIndex, setDragIndex] = useState(-1);
 
+  // const [points, setPoints] = useState([
+  //   {
+  //     x: 50,
+  //     y: 150,
+  //   },
+  //   {
+  //     x: 60,
+  //     y: 100,
+  //   },
+  //   {
+  //     x: 150,
+  //     y: 150,
+  //   },
+  //   {
+  //     x: 160,
+  //     y: 90,
+  //   },
+  //   {
+  //     x: 300,
+  //     y: 150,
+  //   },
+  //   {
+  //     x: 310,
+  //     y: 80,
+  //   },
+  // ]);
   const [points, setPoints] = useState([
-    {
-      x: 50,
-      y: 150,
-      width: 30,
-      height: 30,
-      rad: 16,
-      isDragging: false,
-    },
-    {
-      x: 60,
-      y: 100,
-      width: 30,
-      height: 30,
-      rad: 16,
-      isDragging: false,
-    },
-    {
-      x: 150,
-      y: 150,
-      width: 30,
-      height: 30,
-      rad: 16,
-      isDragging: false,
-    },
-    {
-      x: 160,
-      y: 90,
-      width: 30,
-      height: 30,
-      rad: 16,
-      isDragging: false,
-    },
-    {
-      x: 300,
-      y: 150,
-      width: 30,
-      height: 30,
-      rad: 16,
-      isDragging: false,
-    },
-    {
-      x: 310,
-      y: 80,
-      width: 30,
-      height: 30,
-      rad: 16,
-      isDragging: false,
-    },
+    { x: 50, y: 150, child: [1] },
+    { x: 60, y: 100, child: null },
+    { x: 150, y: 150, child: null },
+    { x: 160, y: 90, child: [2, -1] },
+    { x: 300, y: 150, child: null },
+    { x: 310, y: 80, child: [4] },
   ]);
 
   // useEffect(() => {
@@ -81,21 +67,49 @@ const BeziControlsAlt = () => {
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas?.getContext('2d');
-    const colors = ['red', 'green', 'blue'];
+    const colors = ['red', 'pink', 'green', 'lightgreen', 'blue', 'lightblue'];
     const drawShapes = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       points.forEach((p, i) => {
         ctx.fillStyle = colors[i];
         ctx.beginPath();
-        ctx.arc(p.x, p.y, p.rad, 0, 2 * Math.PI);
+        ctx.arc(p.x, p.y, rad, 0, 2 * Math.PI);
         ctx.fill();
       });
+      // points.forEach((pt) => {
+      //   // Circle around anchors and control points
+      //   ctx.strokeStyle = '#00ffff';
+      //   ctx.fillStyle = '#00ffffff';
+      //   ctx.beginPath();
+      //   ctx.arc(pt.x, pt.y, rad, 0, 2 * Math.PI);
+      //   ctx.stroke();
+      //   if (pt.child) {
+      //     ctx.fill();
+      //   }
+      // });
     };
 
     if (ctx) {
       drawShapes();
     }
-  }, [points]);
+  }, [dragIndex, points]);
+
+  // const handleMouseDown = (e) => {
+  //   const canvas = canvasRef.current;
+  //   const rect = canvas.getBoundingClientRect();
+  //   const mouseX = e.clientX - rect.left;
+  //   const mouseY = e.clientY - rect.top;
+  //   if (
+  //     mouseX >= shape.x &&
+  //     mouseX <= shape.x + shape.width &&
+  //     mouseY >= shape.y &&
+  //     mouseY <= shape.y + shape.height
+  //   ) {
+  //     setIsDragging(true);
+  //     setOffsetX(mouseX - shape.x);
+  //     setOffsetY(mouseY - shape.y);
+  //   }
+  // };
 
   const handleMouseDown = (e) => {
     const canvas = canvasRef.current;
@@ -104,26 +118,18 @@ const BeziControlsAlt = () => {
     const mouseY = e.clientY - rect.top;
 
     const nearestIndex = getNearest(mouseX, mouseY);
-    // console.log('nearestIndex', nearestIndex);
-    setDragIndex(nearestIndex);
-
-    if (
-      mouseX >= shape.x &&
-      mouseX <= shape.x + shape.width &&
-      mouseY >= shape.y &&
-      mouseY <= shape.y + shape.height
-    ) {
-      setIsDragging(true);
-      setOffsetX(mouseX - shape.x);
-      setOffsetY(mouseY - shape.y);
+    if (nearestIndex !== -1) {
+      setDragIndex(nearestIndex);
+      setOffsetX(mouseX - points[nearestIndex].x);
+      setOffsetY(mouseY - points[nearestIndex].y);
     }
   };
 
   const getNearest = (mouseX: number, mouseY: number) => {
     let dx = mouseX - points[0].x;
-    let dy = mouseX - points[0].x;
+    let dy = mouseY - points[0].y;
     let d = Math.sqrt(dx * dx + dy * dy);
-    let index = 0;
+    let index = d <= rad ? 0 : -1;
 
     for (let i = 1; i < points.length; i++) {
       dx = mouseX - points[i].x;
@@ -135,26 +141,56 @@ const BeziControlsAlt = () => {
       }
     }
 
-    return index;
+    if (d <= rad) {
+      return index;
+    }
+
+    return -1;
   };
 
+  // const handleMouseMove = (e) => {
+  //   if (isDragging) {
+  //     const canvas = canvasRef.current;
+  //     const rect = canvas.getBoundingClientRect();
+  //     const mouseX = e.clientX - rect.left;
+  //     const mouseY = e.clientY - rect.top;
+
+  //     setShape((prevShape) => ({
+  //       ...prevShape,
+  //       x: mouseX - offsetX,
+  //       y: mouseY - offsetY,
+  //     }));
+  //   }
+  // };
+
   const handleMouseMove = (e) => {
-    if (isDragging) {
+    if (dragIndex !== -1) {
       const canvas = canvasRef.current;
       const rect = canvas.getBoundingClientRect();
       const mouseX = e.clientX - rect.left;
       const mouseY = e.clientY - rect.top;
 
-      setShape((prevShape) => ({
-        ...prevShape,
-        x: mouseX - offsetX,
-        y: mouseY - offsetY,
-      }));
+      const updated = points.map((pt, i) => {
+        if (i !== dragIndex) {
+          return pt;
+        }
+        return {
+          ...pt,
+          x: mouseX - offsetX,
+          y: mouseY - offsetY,
+        };
+      });
+
+      setPoints(updated);
     }
   };
 
+  // const handleMouseUp = () => {
+  //   setIsDragging(false);
+  // };
+
   const handleMouseUp = () => {
-    setIsDragging(false);
+    setDragIndex(-1);
   };
 
   return (
