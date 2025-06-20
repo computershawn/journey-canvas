3aacd0a83b057f327680c22fdd1ab96e4e677459
computershawn <shontek@gmail.com>
2025-06-20 15:08:47 -0700

    Add tsx typechecking, clean up helpers utils

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,18 +1,16 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
-// TODO: Add typings to this file
+import { CompValues } from '../types';
 
 // EASING FUNCTIONS
-export const easeInCubic = (t) => {
-  return t * t * t;
-};
+// export const easeInCubic = (t: number) => {
+//   return t * t * t;
+// };
 
-export const easeInQuad = (t) => {
-  return t * t;
-};
+// export const easeInQuad = (t: number) => {
+//   return t * t;
+// };
 
 // TAKE A VALUE AND OUTPUT A RANDOM NEW VALUE WITHIN +/- RANGE
-export const wobble = (val, range) => {
+export const wobble = (val: number, range: number) => {
   const dir = Math.random() > 0.5 ? 1 : -1;
   const variance = 1.0 + dir * Math.random() * range;
   const wob = variance * val;
@@ -24,7 +22,13 @@ export const wobble = (val, range) => {
 };
 
 // CALCULATE THE X/Y COORDINATES ALONG AN ARC
-export const getArcPoints = (arcAngle, numPoints, r, cx = 0, cy = 0) => {
+export const getArcPoints = (
+  arcAngle: number,
+  numPoints: number,
+  r: number,
+  cx = 0,
+  cy = 0
+) => {
   const temp = [];
   for (let i = 0; i < numPoints; i++) {
     const a = ((2 * i) / numPoints) * arcAngle - arcAngle - Math.PI / 2;
@@ -43,14 +47,14 @@ export const getRandomIndex = (len: number) => {
 };
 
 // JUMP TO A SPECIFIC FRAME NUMBER IN THE UNFURLY ANIMATION
-export const goToFrameNumber = (frameNum) => {
-  if (animationMode === 1) {
-    currentCycleFrame = frameNum % durationFrames;
-  }
-};
+// export const goToFrameNumber = (frameNum: number) => {
+//   if (animationMode === 1) {
+//     currentCycleFrame = frameNum % durationFrames;
+//   }
+// };
 
 // REMOVE COMPOSITION SETTINGS
-export const removeComp = (compID) => {
+export const removeComp = (compID: string) => {
   const temp = [...getAllComps()];
   const updatedComps = temp.filter((item) => item.id !== compID);
 
@@ -60,14 +64,10 @@ export const removeComp = (compID) => {
 export const getAllComps = (): CompValues[] => {
   const savedComps = window.localStorage.getItem('saved_comps');
 
-  if (!savedComps) {
-    return [];
-  }
-
-  return JSON.parse(savedComps);
+  return (savedComps ? JSON.parse(savedComps) : []) as CompValues[];
 };
 
-export const getComp = (compObj) => {
+export const getComp = (compObj: CompValues) => {
   const keys = Object.keys(compObj);
   const hasAllKeys = ['balance', 'curveSetPoints', 'diff', 'id'].every((item) =>
     keys.includes(item)
@@ -86,66 +86,66 @@ export const getComp = (compObj) => {
 };
 
 // DROPDOWN
-export const toggleDropdown = () => {
-  document.getElementById('comp-select').classList.toggle('show');
-};
+// export const toggleDropdown = () => {
+//   document.getElementById('comp-select').classList.toggle('show');
+// };
 
 // CLOSE THE DROPDOWN IF THE USER CLICKS OUTSIDE OF IT
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    const dropdowns = document.getElementsByClassName('dropdown-content');
-    for (let i = 0; i < dropdowns.length; i++) {
-      const openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-};
+// window.onclick = function (event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     const dropdowns = document.getElementsByClassName('dropdown-content');
+//     for (let i = 0; i < dropdowns.length; i++) {
+//       const openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// };
 
-export const initPalette = async () => {
-  const allCo = await getColors();
+// export const initPalette = async () => {
+//   const allCo = await getColors();
 
-  return allCo;
-};
+//   return allCo;
+// };
 
-export const pickPalette = (allColorsArray) => {
-  const randomIndex = getRandomIndex(allColorsArray.length);
-  const temp = allColorsArray[randomIndex];
+// export const pickPalette = (allColorsArray) => {
+//   const randomIndex = getRandomIndex(allColorsArray.length);
+//   const temp = allColorsArray[randomIndex];
 
-  const swatchDivs = document.querySelectorAll('.swatch');
-  swatchDivs.forEach((s, i) => {
-    s.style.backgroundColor = temp[i];
-  });
+//   const swatchDivs = document.querySelectorAll('.swatch');
+//   swatchDivs.forEach((s, i) => {
+//     s.style.backgroundColor = temp[i];
+//   });
 
-  return temp;
-};
+//   return temp;
+// };
 
-export const renderFan = (fanBladesArr, nullRefs) => {
-  // Draw our lines based on the updated references
-  for (let j = 0; j < nullRefs.length - 1; j++) {
-    const thisRef = nullRefs[j];
-    const nextRef = nullRefs[j + 1];
+// export const renderFan = (fanBladesArr, nullRefs) => {
+//   // Draw our lines based on the updated references
+//   for (let j = 0; j < nullRefs.length - 1; j++) {
+//     const thisRef = nullRefs[j];
+//     const nextRef = nullRefs[j + 1];
 
-    const px0 = thisRef.point0.x + thisRef.x;
-    const py0 = thisRef.point0.y + thisRef.y;
-    const px1 = thisRef.point1.x + thisRef.x;
-    const py1 = thisRef.point1.y + thisRef.y;
-    const px2 = nextRef.point1.x + nextRef.x;
-    const py2 = nextRef.point1.y + nextRef.y;
-    const px3 = nextRef.point0.x + nextRef.x;
-    const py3 = nextRef.point0.y + nextRef.y;
+//     const px0 = thisRef.point0.x + thisRef.x;
+//     const py0 = thisRef.point0.y + thisRef.y;
+//     const px1 = thisRef.point1.x + thisRef.x;
+//     const py1 = thisRef.point1.y + thisRef.y;
+//     const px2 = nextRef.point1.x + nextRef.x;
+//     const py2 = nextRef.point1.y + nextRef.y;
+//     const px3 = nextRef.point0.x + nextRef.x;
+//     const py3 = nextRef.point0.y + nextRef.y;
 
-    const fb = fanBladesArr[j];
-    const pv0 = { x: px0, y: py0 };
-    const pv1 = { x: px1, y: py1 };
-    const pv2 = { x: px2, y: py2 };
-    const pv3 = { x: px3, y: py3 };
+//     const fb = fanBladesArr[j];
+//     const pv0 = { x: px0, y: py0 };
+//     const pv1 = { x: px1, y: py1 };
+//     const pv2 = { x: px2, y: py2 };
+//     const pv3 = { x: px3, y: py3 };
 
-    fb.update(pv0, pv1, pv2, pv3);
-    fb.render();
-  }
-};
+//     fb.update(pv0, pv1, pv2, pv3);
+//     fb.render();
+//   }
+// };
 
 export const mapTo = (
   value: number,
@@ -159,14 +159,14 @@ export const mapTo = (
   return toMin + amount * (toMax - toMin);
 };
 
-export const throttle = (callback, interval) => {
-  let enableCall = true;
+// export const throttle = (callback, interval) => {
+//   let enableCall = true;
 
-  return function (...args) {
-    if (!enableCall) return;
+//   return function (...args) {
+//     if (!enableCall) return;
 
-    enableCall = false;
-    callback.apply(this, args);
-    setTimeout(() => (enableCall = true), interval);
-  };
-};
+//     enableCall = false;
+//     callback.apply(this, args);
+//     setTimeout(() => (enableCall = true), interval);
+//   };
+// };
