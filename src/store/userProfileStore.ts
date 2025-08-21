b900502c46e4c3b2f32b2cb2fe32e14f3a2449b5
computershawn <shontek@gmail.com>
2025-08-20 22:36:35 -0700

    Remove unused userProfileStore

--- src/store/userProfileStore.ts
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-const useUserProfileStore = create((set) => ({
-  userProfile: null,
-  setUserProfile: (profile) => set({ userProfile: profile }),
-  // addPost: (post) =>
-}));
-
-export default useUserProfileStore;
