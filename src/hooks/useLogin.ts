13cf47cd4ead5893956c9d722d311444640932e9
computershawn <shontek@gmail.com>
2025-09-17 00:54:46 -0700

    Clean up useLogin

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,20 +1,19 @@
 'use client';
 
-import { doc, getDoc } from 'firebase/firestore';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
-import { auth, firestore } from '../firebase';
-import useAuthStore from '../store/authStore';
-import { saveToLocalStorage } from '../utils/storageOps';
+import { auth } from '../firebase';
 import { loggy } from '../utils/helpers';
 
+// This useLogin custom hook is a simplified version of useLogin from
+// https://github.com/computershawn/antsigarm/blob/main/src/hooks/useLogin.ts
+// In that project, we save information to local storage and store additional
+// user data in a 'users' Firestore collection. If it turns out that we need
+// to store additional user data, that project can be used as a reference.
 export const useLogin = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [signInWithEmailAndPassword, loggedInUser, loading, error] =
     useSignInWithEmailAndPassword(auth);
-
-  // @ts-expect-error 'state' is of type 'unknown'
-  const loginUser = useAuthStore((state) => state.login);
 
   // @ts-expect-error Parameter 'inputs' implicitly has an 'any' type
   const login = async (inputs) => {
@@ -23,16 +22,7 @@ export const useLogin = () => {
     }
 
     try {
-      const userCred = await signInWithEmailAndPassword(
-        inputs.email,
-        inputs.password,
-      );
-      if (userCred) {
-        const docRef = doc(firestore, 'users', userCred.user.uid);
-        const docSnap = await getDoc(docRef);
-        saveToLocalStorage('journey_user', docSnap.data());
-        loginUser(docSnap.data());
-      }
+      await signInWithEmailAndPassword(inputs.email, inputs.password);
     } catch (error) {
       loggy.error('Login failed', error);
     }
