c4ed35cb803fcd413e22bbf474d99748b4681fbf
computershawn <shontek@gmail.com>
2026-04-01 21:29:39 -0700

    Import nice-color-palettes instead of fetching it

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module 'nice-color-palettes' {
+  const palettes: string[][];
+  export default palettes;
+}
+
+declare module 'nice-color-palettes/200' {
+  const palettes: string[][];
+  export default palettes;
+}
